e9a680a7bea5fc72adb9f3d39e7e51afd32beda8
ChandaraLeang <leangchandara@gmail.com>
2017-08-07 22:47:33 -0500

    Create new IMDB Project Extra Credit

---
+++ EA_Extra_Credit_2/src/main/java/cs544/mum/edu/EA_Extra_Credit_2/App.java
@@ -0,0 +1,15 @@
+package cs544.mum.edu.EA_Extra_Credit_2;
+
+/**
+ * Name: CHANDARA LEANG
+ * ID: 108619
+ * Task: Extra Credit 2
+ * Class: App
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
